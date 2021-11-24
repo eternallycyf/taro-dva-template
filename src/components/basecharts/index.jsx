19c5a39d4c612e164d6e7f79f1beeb65b3763836
Eternallycyf <969475322@qq.com>
2021-11-24 09:54:42 +0800

    修复echarts异步问题

--- src/components/basecharts/index.jsx
+++ src/components/basecharts/index.jsx
@@ -1,24 +1,5 @@
 import React from 'react'
 import * as echarts from '@/components/ec-canvas/echarts'
-/**
- * 
- * @param {Object} option 图表的配置项
- * @param {number} w canvas画布的宽度
- * @param {number} h canvas画布的高度
- */
-function init(option, w, h) {
-  return {
-    onInit: (canvas, width, height) => {
-      const chart = echarts.init(canvas, null, {
-        width: w ? w : width,
-        height: h ? h : height
-      })
-      canvas.setChart(chart)
-      chart.setOption(option)
-      return chart
-    }
-  }
-}
 
 /**
  * <BaseCharts option={option} type={'mychart-area'} />
@@ -26,19 +7,38 @@ function init(option, w, h) {
  * @property {string | number} width 画布的宽度
  * @property {string | number} height 画布的高度
  * @property {string} type 图表的类型
- * @property {any} restProps 剩余参数
  * @returns 
  */
 const BaseCharts = ({
   option = {},
   width = 200,
   height = 200,
-  type = 'mychart-area',
-  ...restProps }) => {
+  type = 'mychart-area'
+}) => {
+
+  /**
+   * 
+   * @param {Object} option 图表的配置项
+   * @param {number} w canvas画布的宽度
+   * @param {number} h canvas画布的高度
+   */
+  const init = (option, w, h) => {
+    return {
+      onInit: (canvas, width, height) => {
+        const chart = echarts.init(canvas, null, {
+          width: w ? w : width,
+          height: h ? h : height
+        })
+        canvas.setChart(chart)
+        chart.setOption(option)
+        return chart
+      }
+    }
+  }
+
   return (
     <>
       <ec-canvas
-        {...restProps}
         canvas-id={type}
         ec={init(option, width, height)}
       />
