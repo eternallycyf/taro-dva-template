14bf18ae6e3ddb6757698ffaa68653b3d3002838
eternallycyf <a969475322@gmail.com>
2022-08-15 09:15:31 +0800

    fix: multiple const

--- src/utils/http/utils.js
+++ src/utils/http/utils.js
@@ -25,31 +25,3 @@ export const isLogin = () => {
   const token = Taro.getStorageSync('Authorization');
   return !!token;
 };
-
-import Taro from '@tarojs/taro';
-/**
- * @description 获取当前页url
- */
-export const getCurrentPageUrl = () => {
-  let pages = Taro.getCurrentPages();
-  let currentPage = pages[pages.length - 1];
-  let url = currentPage.route;
-  return url;
-};
-
-export const pageToLogin = () => {
-  let path = getCurrentPageUrl();
-  if (!path || !path.includes('login')) {
-    Taro.navigateTo({
-      url: '/pages/login/index',
-    });
-  }
-};
-
-/**
- * @description 判断登陆状态
- */
-export const isLogin = () => {
-  const token = Taro.getStorageSync('Authorization');
-  return !!token;
-};
