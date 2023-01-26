4516dbb2be44340e977a11bfbe42bb05b9f2d40b
eternallycyf <a969475322@gmail.com>
2023-01-26 19:34:26 +0800

    chore: init script

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,13 @@
-import index from '@/pages/index/model';
-import list from '@/pages/list/model';
+//本文件是自动生成，请勿修改
+import echarts from '../pages/echarts/model'; 
+import form from '../pages/form/model'; 
+import index from '../pages/index/model'; 
+import list from '../pages/list/model'; 
 
-export default [index, list];
+export default [
+  echarts,
+  form,
+  index,
+  list,
+]
+
